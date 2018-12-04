3c20f8613f2bcd0cbdb4afa490e315f034496b50
masisg <masis.gulmez@hetic.net>
2018-12-05 00:05:09 +0100

    wip products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,22 +1,20 @@
 module ProductsHelper
-	def self.products(parameter)
+	def self.products
 		@product_array = Array.new
 
-		if parameter === 'all'
-			@prod = Product.all
+		@prod = Product.all
 
-			@prod.each do |p|
-				id         = p.id
-				name       = p.name
-				quantity   = p.quantity
-				reference  = p.reference
-				company_id = p.companies_id.to_s.html_safe
-				company    = Company.find(company_id).name
+		@prod.each do |p|
+			id         = p.id
+			name       = p.name
+			quantity   = p.quantity
+			reference  = p.reference
+			company_id = p.companies_id.to_s.html_safe
+			company    = Product.company_name(company_id).name
 
-				@tab = Product.new(name: name, quantity: quantity, reference: reference, company: company, id: id)
-				@product_array.push(@tab)
-			end
-			return @product_array
+			@tab = Product.new(:name => name, :quantity => quantity, :reference => reference, :company => company, :id => id)
+			@product_array.push(@tab)
 		end
+		return @product_array
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
 	belongs_to :company, optional: true, dependent: :destroy
 	attr_accessor :company
+
+	def self.company_name(id)
+		Company.find(id)
+	end
+
+	def self.show_one_product(id)
+		@product = {}
+		@product[:product] = Product.find(id)
+		@company = Company.find(@product[:product].companies_id)
+		@product[:company] = @company.name
+
+		return @product
+	end
 end
