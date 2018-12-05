a92138d39eaf6629cf70879354da2d6cd1fc2465
masisg <masis.gulmez@hetic.net>
2018-12-05 15:56:05 +0100

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root to: 'companies#index'
+	root to: 'homes#index'
 	resources :products
 	resources :companies
 	resources :users
