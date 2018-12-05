294b23d6c03110361f825c1357d9e9de86d7769e
masisg <masis.gulmez@hetic.net>
2018-12-05 18:28:32 +0100

    display lists on company details

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,17 @@
 class Company < ApplicationRecord
 	has_many :products, foreign_key: 'companies_id', dependent: :destroy
 	has_many :users, foreign_key: 'companies_id', dependent: :destroy
+
+	def self.display_products(id)
+		@product_array = Array.new
+		@product_array = Product.where(:companies_id => id)
+		return @product_array
+	end
+
+	def self.display_users(id)
+		@users_array = Array.new
+		@users_array = User.where(:companies_id => id)
+		return @users_array
+	end
+
 end
